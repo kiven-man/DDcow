2c42ac8c8021e21050db33986585dae6621034c6
suxsoft <suxsoft@suxsoftdeMacBook-Pro.local>
2014-11-10 17:46:12 +0800

    Initial Commit

---
+++ DDcow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DDcow
+//
+//  Created by suxsoft on 14-11-10.
+//  Copyright (c) 2014年 suxsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DDcow/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DDcow
+//
+//  Created by suxsoft on 14-11-10.
+//  Copyright (c) 2014年 suxsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
